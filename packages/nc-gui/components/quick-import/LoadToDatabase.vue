e96426d4cf3b5eea413ded948ce346c55016c83e
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-06 17:35:54 +0800

    feat(nc-gui): styling LoadToDatabase

--- packages/nc-gui/components/quick-import/LoadToDatabase.vue
+++ packages/nc-gui/components/quick-import/LoadToDatabase.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 function append() {
   // TODO:
-  console.log('Append')
 }
 
 function merge() {
@@ -16,19 +15,25 @@ function overwrite() {
 <template>
   <a-row :gutter="16">
     <a-col :span="8">
-      <a-card :title="$t('activity.import.loadToDataBase.append')" :bordered="false" class="cursor-pointer" @click="append">
+      <a-card :title="$t('activity.import.loadToDataBase.append')" class="nc-import-action-button" @click="append">
         <p>{{ $t('activity.import.loadToDataBase.appendDesc') }}</p>
       </a-card>
     </a-col>
     <a-col :span="8">
-      <a-card :title="$t('activity.import.loadToDataBase.merge')" :bordered="false" class="cursor-pointer" @click="merge">
+      <a-card :title="$t('activity.import.loadToDataBase.merge')" class="nc-import-action-button" @click="merge">
         <p>{{ $t('activity.import.loadToDataBase.mergeDesc') }}</p>
       </a-card>
     </a-col>
     <a-col :span="8">
-      <a-card :title="$t('activity.import.loadToDataBase.overwrite')" :bordered="false" class="cursor-pointer" @click="overwrite">
+      <a-card :title="$t('activity.import.loadToDataBase.overwrite')" class="nc-import-action-button" @click="overwrite">
         <p>{{ $t('activity.import.loadToDataBase.overwriteDesc') }}</p>
       </a-card>
     </a-col>
   </a-row>
 </template>
+
+<style lang="scss">
+.nc-import-action-button {
+  @apply shadow-md shadow-gray-400 ring-opacity-100 cursor-pointer;
+}
+</style>
